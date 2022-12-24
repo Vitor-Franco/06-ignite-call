2c249dca87f18cfcd93cb9a517a367e6ec6f8a3e
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-12-24 09:34:41 -0300

    feat: adiciona estilizacoes e estrutura da pagina home

---
+++ src/pages/home/styles.ts
@@ -0,0 +1,38 @@
+import { styled, Heading, Text } from '@ignite-ui/react'
+
+export const Container = styled('div', {
+  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
+  // Calculo utilizado para colocar uma margem dinâmica a esquerda e mantendo o outro lado do container colado na tela, sendo o calculo
+  // 1. Toda viewport - ((toda viewport - espaço reservado para o conteudo) = o espaço n utilizado) / 2,
+  // 2. divimos para dois pois só removemos a margem de um lado
+  marginLeft: 'auto',
+  height: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$20',
+})
+
+export const Hero = styled('div', {
+  maxWidth: 480,
+  padding: '0 $10',
+
+  [`> ${Heading}`]: {
+    '@media(max-width: 600px)': {
+      fontSize: '$6xl',
+    },
+  },
+
+  [`> ${Text}`]: {
+    marginTop: '$2',
+    color: '$gray200',
+  },
+})
+
+export const Preview = styled('div', {
+  paddingRight: '$8',
+  overflow: 'hidden',
+
+  '@media(max-width: 600px)': {
+    display: 'none',
+  },
+})
