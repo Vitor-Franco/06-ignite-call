129aa7478edc63dccea5d443d4ccf73af500ad58
Vitor Franco <vitoroliveirafranco@gmail.com>
2023-01-03 17:03:17 -0300

    feat: adiciona formulario para agendamento com o comp de calendario

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,60 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '../../../lib/prisma'
+import { ScheduleForm } from './ScheduleForm'
+import { Container, UserHeader } from './styles'
+
+interface ScheduleProps {
+  user: string
+  bio: string
+  avatarUrl: string
+}
+
+export default function Schedule({ user, avatarUrl, bio }: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={avatarUrl} />
+        <Heading>{user}</Heading>
+        <Text>{bio}</Text>
+      </UserHeader>
+
+      <ScheduleForm />
+    </Container>
+  )
+}
+
+// Informa para o next quais serão os paths que serão gerados no momento do build.
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [], // Não temos nenhum path pré-definido, então deixamos vazio. Será gerado no momento da requisição
+    fallback: 'blocking', // Se não existir o path, o next irá gerar no momento da requisição.
+  }
+}
+
+// Sempre executa do lado do servidor, portanto podemos trabalhar com informações do back-end e dados sensiveis.
+// É gerado no momento do build, portanto não tem acesso a req, res.
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: user.name,
+      bio: user.bio,
+      avatarUrl: user.avatar_url,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
+}
--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,7 +1,17 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
-    .map((day) => day.charAt(0).toUpperCase() + day.slice(1))
+    .map((day) => {
+      if (short) {
+        return day.substring(0, 3).toUpperCase()
+      }
+
+      return day.charAt(0).toUpperCase() + day.slice(1)
+    })
 }
